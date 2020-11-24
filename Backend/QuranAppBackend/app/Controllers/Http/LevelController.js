02c8652e5222931ba042e764662e74ba0bae8198
samer abd alla <smsssamer@live.com>
2020-11-25 00:13:06 +0300

    create Controllers

---
+++ Backend/QuranAppBackend/app/Controllers/Http/LevelController.js
@@ -0,0 +1,15 @@
+"use strict";
+
+class LevelController {
+  async index() {}
+
+  async Create() {}
+
+  async Update() {}
+
+  async Remove() {}
+
+  async Details() {}
+}
+
+module.exports = LevelController;
